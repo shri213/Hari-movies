a750dcec9f5fbe82650d6dfb0ed35a01ff471151
shri213 <137676125+shri213@users.noreply.github.com>
2024-02-15 08:40:39 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-Auto-Filter /Advance-Auto-Filter
+  git clone shri213/Hari-movies /Hari-movies
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Advance-Auto-Filter
+  git clone $UPSTREAM_REPO /Hari-movies
 fi
-cd /Advance-Auto-Filter
+cd /Hari-movies
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
