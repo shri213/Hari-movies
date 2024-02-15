6e6e6c0f8134ecbf941a70f256d20579e021e344
shri213 <137676125+shri213@users.noreply.github.com>
2024-02-15 10:01:58 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone shri213/Hari-movies /Hari-movies
+  git clone Hari-movies /Hari-movies
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Hari-movies
